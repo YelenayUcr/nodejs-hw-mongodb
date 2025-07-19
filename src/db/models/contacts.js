5cd6654ab37c9caf0d8db58ba4bdcaacda5c0039
Yelenay Uçar <yelenayucr@gmail.com>
2025-07-19 19:58:58 +0300

    Update contacts.js

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -1,9 +1,24 @@
 import { Schema, model } from 'mongoose';
 
 const contactSchema = new Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: { 
+    type: String,
+    required: true,
+  },
   email: String,
-  phone: String,
+  isFavourite: {
+    type: Boolean,
+    default: false,
+  },
+  contactType: {
+    type: String,
+    enum: ['work', 'family', 'friends'],
+    required: true,
+  },
 });
 
 const Contact = model('Contact', contactSchema);
